9fb30f0358b4b8b0fb5cce474ab03d3fd08a7e11
Mitch Hunter <144734462+utahphotohunter@users.noreply.github.com>
2025-04-29 22:41:11 -0600

    changed image reference for App.jsx

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,3 +1,5 @@
+import reactImg from './assets/react-core-concepts.png'
+
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function genRandomInt(max) {
@@ -5,12 +7,15 @@ function genRandomInt(max) {
 }
 
 function Header() {
+  const description =
+    reactDescriptions[genRandomInt(reactDescriptions.length - 1)];
+
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
