734b6d03352125bfe34e7c64fbf59d14aa876446
Mitch Hunter <144734462+utahphotohunter@users.noreply.github.com>
2025-05-03 18:55:21 -0600

    grouped components into files for easy maintainence

---
+++ 01-starting-project/src/components/CoreConcept/CoreConcept.jsx
@@ -0,0 +1,11 @@
+import "./CoreConcept.css";
+
+export default function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.img} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
--- 01-starting-project/src/data.js
+++ 01-starting-project/src/data.js
@@ -1,31 +1,31 @@
-import componentsImg from './assets/components.png';
-import propsImg from './assets/config.png';
-import jsxImg from './assets/jsx-ui.png';
-import stateImg from './assets/state-mgmt.png';
+import componentsImg from "./assets/components.png";
+import propsImg from "./assets/config.png";
+import jsxImg from "./assets/jsx-ui.png";
+import stateImg from "./assets/state-mgmt.png";
 
 export const CORE_CONCEPTS = [
   {
     image: componentsImg,
-    title: 'Components',
+    title: "Components",
     description:
-      'The core UI building block - compose the user interface by combining multiple components.',
+      "The core UI building block - compose the user interface by combining multiple components.",
   },
   {
     image: jsxImg,
-    title: 'JSX',
+    title: "JSX",
     description:
-      'Return (potentially dynamic) HTML(ish) code to define the actual markup that will be rendered.',
+      "Return (potentially dynamic) HTML(ish) code to define the actual markup that will be rendered.",
   },
   {
     image: propsImg,
-    title: 'Props',
+    title: "Props",
     description:
-      'Make components configurable (and therefore reusable) by passing input data to them.',
+      "Make components configurable (and therefore reusable) by passing input data to them.",
   },
   {
     image: stateImg,
-    title: 'State',
+    title: "State",
     description:
-      'React-managed data which, when changed, causes the component to re-render & the UI to update.',
+      "React-managed data which, when changed, causes the component to re-render & the UI to update.",
   },
 ];
