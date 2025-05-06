62de70e0fd14811f358562a741c4eefdd75d27eb
Mitch Hunter <144734462+utahphotohunter@users.noreply.github.com>
2025-05-05 22:43:29 -0600

    formatted code

--- 01-starting-project/src/components/Tabs.jsx
+++ 01-starting-project/src/components/Tabs.jsx
@@ -1,10 +1,8 @@
 export default function Tabs({ children, buttons }) {
-    return (
-      <>
-        <menu>
-            {buttons}
-        </menu>
-        {children}
-      </>
-    );
+  return (
+    <>
+      <menu>{buttons}</menu>
+      {children}
+    </>
+  );
 }
