12d74f4bb32accd18f551808fda17042e05af688
Mitch Hunter <144734462+utahphotohunter@users.noreply.github.com>
2025-05-14 21:21:59 -0600

    made player name edit button populate an input field depending on current state

--- tic-tac-toe-starting-project/src/components/Player.jsx
+++ tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,11 +1,27 @@
-export default function Player({name, symbol}) {
+import { useState } from "react";
+
+export default function Player({ name, symbol }) {
+  const [isEditing, setIstEditing] = useState(false);
+
+  function handleEditClick() {
+    setIstEditing((editing) => !editing);
+  }
+
+  let playerName = <span className="player-name">{name}</span>;
+  let btnText = "Edit";
+
+  if (isEditing) {
+    playerName = <input type="text" required value={name} />;
+    btnText = "Save";
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{btnText}</button>
     </li>
   );
 }
