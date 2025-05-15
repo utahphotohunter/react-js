77f3082f4704a2d61f7c6124de7b9c046cf68424
Mitch Hunter <144734462+utahphotohunter@users.noreply.github.com>
2025-05-14 21:37:49 -0600

    made player name customizable

--- tic-tac-toe-starting-project/src/components/Player.jsx
+++ tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,24 +1,31 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIstEditing] = useState(false);
 
   function handleEditClick() {
     setIstEditing((editing) => !editing);
   }
 
-  let playerName = <span className="player-name">{name}</span>;
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
   let btnText = "Edit";
 
   if (isEditing) {
-    playerName = <input type="text" required value={name} />;
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
     btnText = "Save";
   }
 
   return (
     <li>
       <span className="player">
-        {playerName}
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
       <button onClick={handleEditClick}>{btnText}</button>
